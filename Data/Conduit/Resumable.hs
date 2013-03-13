joeyadams/hs-conduit-resumable

commit 01a137b0fe541aef0e78e9507343c7b01e4904ac

Implement '=$+-' without embedFinalizer
The 'embedFinalizer' solution would have dropped the finalizer were the
sink not to request any input.  Also, the implementation of '=$+-' now
looks very similar to '$$+-'.

Data/Conduit/Resumable.hs
@@ -21,6 +21,7 @@ module Data.Conduit.Resumable (
 ) where
 
 import Control.Monad
+import Control.Monad.Trans.Class (lift)
 import Data.Conduit
 import Data.Conduit.Internal
 import Data.Void
@@ -158,30 +159,7 @@ newResumableConduit (ConduitM p) = ResumableConduit p (return ())
        => ResumableConduit a m b
        -> Sink b m r
        -> Sink a m r
-(=$+-) rconduit sink = finishResumableConduit rconduit =$= sink
-
--- | Finalize a 'ResumableConduit' by turning it into a 'Conduit'.
--- The resulting 'Conduit' may only be used once.
---
--- This may be used instead of '=$+-' to finalize a 'ResumableConduit'.
-finishResumableConduit :: Monad m => ResumableConduit i m o -> Conduit i m o
-finishResumableConduit (ResumableConduit p final) =
-    ConduitM $ embedFinalizer final p
-
--- | Make the given 'Pipe' run the given finalizer when it finishes,
--- unless it overrides this finalizer by calling 'Data.Conduit.yieldOr'.
-embedFinalizer :: Monad m => m () -> Pipe l i o u m r -> Pipe l i o u m r
-embedFinalizer final0 p0 =
-    go final0 p0
-  where
-    go final p =
-        case p of
-            HaveOutput _ _ _ -> p -- Finalizer overridden.
-                                  -- Downstream will call the new finalizer
-                                  -- if it doesn't use this output value.
-            NeedInput i u -> NeedInput (recurse . i) (recurse . u)
-            Done r        -> PipeM (final >> return (Done r))
-            PipeM mp      -> PipeM (liftM recurse mp)
-            Leftover p' l -> Leftover (recurse p') l
-      where
-        recurse = go final
+(=$+-) rconduit sink = do
+    (ResumableConduit _ final, res) <- rconduit =$++ sink
+    lift final
+    return res

conduit-resumable.cabal
@@ -23,6 +23,7 @@ library
 
     build-depends: base >= 4.3 && < 5
                  , conduit
+                 , transformers
                  , void
 
     ghc-options: -Wall -fwarn-tabs

commit ad5ef2f75394763bb791a95bcaedad5b7d1c69c7

Fix '=$+-' so it sends EOF to the conduit
I broke this in commit 01a137b: "Implement '=$+-' without embedFinalizer"
Now we send EOF through the conduit, and we call the finalizer when the
conduit is not used to exhaustion.

Data/Conduit/Resumable.hs
@@ -140,7 +140,14 @@ newResumableConduit (ConduitM p) = ResumableConduit p (return ())
        => ResumableConduit a m b
        -> Sink b m r
        -> Sink a m (ResumableConduit a m b, r)
-(=$++) (ResumableConduit conduit0 final0) (ConduitM sink0) =
+(=$++) = resumeConduit True
+
+resumeConduit :: Monad m
+              => Bool
+              -> ResumableConduit a m b
+              -> Sink b m r
+              -> Sink a m (ResumableConduit a m b, r)
+resumeConduit bypassEOF (ResumableConduit conduit0 final0) (ConduitM sink0) =
     ConduitM $ goSink final0 conduit0 sink0
   where
     goSink final conduit sink =
@@ -156,10 +163,16 @@ newResumableConduit (ConduitM p) = ResumableConduit p (return ())
     goConduit rp rc final conduit =
         case conduit of
             HaveOutput conduit' final' o -> goSink final' conduit' (rp o)
-            NeedInput left' _lc -> NeedInput (recurse . left')
-                                             (goSink final conduit . rc)
-               -- Forward EOF to sink, but leave the conduit alone
-               -- so it accepts input from the next source.
+            NeedInput left' lc ->
+                NeedInput (recurse . left')
+                          (if bypassEOF then
+                              -- Forward EOF to sink, but leave the conduit
+                              -- alone so it accepts input from the next source.
+                              goSink final conduit . rc
+                           else
+                              -- Send EOF through the conduit like 'pipe' does.
+                              recurse . lc
+                          )
             Done r              -> goSink (return ()) (Done r) (rc r)
             PipeM mp            -> PipeM (liftM recurse mp)
             Leftover conduit' i -> Leftover (recurse conduit') i
@@ -173,6 +186,6 @@ newResumableConduit (ConduitM p) = ResumableConduit p (return ())
        -> Sink b m r
        -> Sink a m r
 (=$+-) rconduit sink = do
-    (ResumableConduit _ final, res) <- rconduit =$++ sink
+    (ResumableConduit _ final, res) <- resumeConduit False rconduit sink
     lift final
     return res

test/main.hs
@@ -55,8 +55,10 @@ main = hspec $ do
         let c0 = CL.groupBy (==) :: Conduit Int IO [Int]
 
         it "doesn't see EOF until termination" $ do
-            (c1, [[1,1],[2,2]]) <- CL.sourceList [1,1,2,2,3,3] $$ c0 =$+ CL.consume
-            [[3,3]] <- CL.sourceList [] $$ c1 =$+- CL.consume
+            (c1, a) <- CL.sourceList [1,1,2,2,3,3] $$ c0 =$+ CL.consume
+            a `shouldBe` [[1,1],[2,2]]
+            b <- CL.sourceList [] $$ c1 =$+- CL.consume
+            b `shouldBe` [[3,3]]
             return ()
 
         it "empty case" $ do